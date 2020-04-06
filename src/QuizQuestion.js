1ebce50412870a2eeb035f1e66b927b8fd4b7473
Alejandro Loayza <ameto79@gmail.com>
2020-04-06 16:57:32 -0400

    module WIP

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
-
 class QuizQuestion extends Component {
     constructor(props){
         super(props);
